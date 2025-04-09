762a515f0a092b475d5660f096e32acc403f0aae
Alejandro Uribe <alexuribemoreno@gmail.com>
2025-04-08 21:06:38 -0600

    API PARA LOGIN
    Funciona en si, pero chance luego le cambie algo para que funcione en unity

---
+++ ApiGame/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace ApiGame.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LoginController : ControllerBase
+{
+    private readonly string _connectionString = "Server=construcciondesoftwate-databaselibroprueba.i.aivencloud.com;Port=15400;Database=oxxodb;Uid=avnadmin;Pwd=AVNS_EbD2wE2Jb0yXJYlPLsE;SslMode=Required;SslCa=ApiGame/ca.pem";
+
+    [HttpGet(Name = "VerifyPassAndUser")]
+    public ActionResult<int?> Get(string user, string contrasena)
+    {
+        int? userId = CheckCredentials(user, contrasena);
+        if (userId.HasValue)
+        {
+            return Ok(userId);
+        }
+        return Unauthorized("Invalid credentials");
+    }
+
+    private int? CheckCredentials(string user, string contrasena)
+    {
+        int? userId = null;
+        using (MySqlConnection conexion = new MySqlConnection(_connectionString))
+        {
+            conexion.Open();
+            MySqlCommand cmd = new MySqlCommand(
+                "SELECT u.id_usuario FROM usuario u INNER JOIN contrasena c ON u.id_contrasena = c.id_contrasena WHERE u.correo = @user AND c.contrasena = @contrasena;", conexion);
+            cmd.Parameters.AddWithValue("@user", user);
+            cmd.Parameters.AddWithValue("@contrasena", contrasena);
+
+            using (var reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    userId = reader.GetInt32("id_usuario");
+                }
+            }
+        }
+        return userId;
+    }
+}
