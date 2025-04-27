c6fcec31aef49a6664fd560e77bd35f5c33e1ccd
Adolfo <A01741767@tec.mx>
2025-04-27 13:20:54 -0600

    ultimas actualizaciones para la tienda

--- ApiGame/Controllers/LoginController.cs
+++ ApiGame/Controllers/LoginController.cs
@@ -143,7 +143,7 @@ public class LoginController : ControllerBase
     }
 
     [HttpPut("Actualizaractivo")]
-    public void activoActual([FromForm] SkinUpdateModel model)
+    public void activoActual([FromBody] SkinUpdateModel model)
     {
 
         using (MySqlConnection conexion = new MySqlConnection(_connectionString))
